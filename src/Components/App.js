0656946ba149978b60026fdc8c0284dedb2bd393
Roland-Sankara <roland1sankara@gmail.com>
2021-05-13 23:24:40 +0300

    add the boiler plate react project

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Input from './Input';
+import Preview from './Preview';
+
+function App(){
+    return(
+        <div>
+            <h1>Markdown Previewer</h1>
+            <p>Loading . . .</p>
+            <Input/>
+            <Preview/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Components/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Input(){
+    return(
+        <textarea id="editor"></textarea>
+    )
+}
+
+export default Input;
